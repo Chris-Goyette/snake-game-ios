72287306482d3e8d0e7693beee30aeb2db55c52c
cybr <cybr@cybrs-Mac-mini.local>
2026-03-10 13:44:37 -0700

    Configure Supabase shared leaderboard keys

--- ios/config.js
+++ ios/config.js
@@ -1,4 +1,4 @@
 window.SNAKE_BACKEND_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://rczxcfxjqwxqpzghfybb.supabase.co',
+  supabaseAnonKey: 'sb_publishable_okqq_H5Gk59sVJHShU-LUg_akY-_nEH',
 };
